5ecae06944db156c5c8d5e2c2bcd85639c5ba5ab
Jaeook Jeong <wodnr0710@naver.com>
2022-03-01 00:15:33 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchsummary import summary
+import torchvision.models as models
+import timm
 
 
 class BaseModel(nn.Module):
@@ -34,6 +37,189 @@ class BaseModel(nn.Module):
         return self.fc(x)
 
 
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False
+    )
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.LeakyReLU(inplace=True)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, num_classes, block=ResidualBlock, layers=[2, 2, 2]):
+        super(ResNet, self).__init__()
+        self.in_channels = 16
+        self.conv = conv3x3(3, 16)
+        self.bn = nn.BatchNorm2d(16)
+        self.relu = nn.LeakyReLU(inplace=True)
+        self.layer1 = self.make_layer(block, 16, layers[0])
+        self.layer2 = self.make_layer(block, 32, layers[1], 2)
+        self.layer3 = self.make_layer(block, 64, layers[2], 2)
+        self.avg_pool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(12288, num_classes)
+
+    def make_layer(self, block, out_channels, num_blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(out_channels),
+            )
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, num_blocks):
+            layers.append(block(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn(out)
+        out = self.relu(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)
+        # print(out.shape)
+        out = self.fc(out)
+        return out
+
+
+class WiderBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(WiderBlock, self).__init__()
+        self.residual = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, out_channels, 3, stride=stride, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Conv2d(out_channels, out_channels, 3, stride=1, padding=1),
+        )
+
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Conv2d(
+                in_channels, out_channels, 1, stride=stride, padding=0
+            )
+
+    def forward(self, x):
+        x_shortcut = self.shortcut(x)
+        x_res = self.residual(x)
+        return x_shortcut + x_res
+
+
+class WRN(nn.Module):
+    def __init__(self, num_classes, depth=40, k=2, init_weights=True):
+        super().__init__()
+        N = int((depth - 4) / 6)
+        self.in_channels = 16
+
+        self.conv1 = nn.Conv2d(3, 16, 3, stride=1, padding=1)
+        self.conv2 = self._make_layer(16 * k, N, 1)
+        self.conv3 = self._make_layer(32 * k, N, 2)
+        self.conv4 = self._make_layer(64 * k, N, 2)
+        self.bn = nn.BatchNorm2d(64 * k)
+        self.relu = nn.ReLU()
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64 * k, num_classes)
+
+        # weight initialization
+        if init_weights:
+            self._weights_initialize()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+    def _make_layer(self, out_channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+
+        for stride in strides:
+            layers.append(WiderBlock(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    # weight initialization
+    def _weights_initialize(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+
+class Res18Model(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.res18 = models.resnet18(pretrained=True)
+        self.res18.fc = nn.Sequential(
+            # nn.Linear(2048, 1024),
+            #  nn.Linear(1024, 256),
+            nn.Linear(512, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.res18(x)
+        return x
+
+
+class Efficient_b4(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.eff_b4 = timm.create_model("tf_efficientnet_b4", pretrained=True)
+        self.eff_b4.classifier = nn.Sequential(
+            nn.Linear(1792, 512),
+            #  nn.Linear(1024, 256),
+            nn.Linear(512, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.eff_b4(x)
+        return x
+
+
 # Custom Model Template
 class MyModel(nn.Module):
     def __init__(self, num_classes):
@@ -51,3 +237,12 @@ class MyModel(nn.Module):
         2. 결과로 나온 output 을 return 해주세요
         """
         return x
+
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# model = WRN(num_classes=18, k=10).to(device)
+# x = torch.randn(3, 3, 64, 64).to(device)
+# output = model(x)
+# print(output.size())
+
+# summary(model, (3, 64, 64), device=device.type)
