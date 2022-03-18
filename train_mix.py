613b6816b37c1051179d259bc8a7901207bb1a98
hyunzzin <forinpea121@gmail.com>
2022-03-18 04:06:09 +0000

    all train.py upload

--- train_mix.py
+++ train_mix.py
@@ -13,27 +13,72 @@ from sklearn.model_selection import StratifiedKFold
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from torch.optim.lr_scheduler import StepLR, CosineAnnealingWarmRestarts, CyclicLR,ExponentialLR, CosineAnnealingLR,ReduceLROnPlateau, CosineAnnealingWarmUpRestarts
+from torch.optim.lr_scheduler import StepLR, CosineAnnealingWarmRestarts, CyclicLR,ExponentialLR, CosineAnnealingLR,ReduceLROnPlateau
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
+from torch.nn import CrossEntropyLoss
 
 from dataset import MaskBaseDataset
 from loss import create_criterion
 from sklearn.metrics import f1_score
 import wandb
-import cv2
 # from .radam import RAdam
-from pytorch_grad_cam import (
-    GradCAM,
-    ScoreCAM,
-    GradCAMPlusPlus,
-    AblationCAM,
-    XGradCAM,
-    EigenCAM,
-    FullGrad,
-)
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
-from pytorch_grad_cam.utils.image import show_cam_on_image
+
+def rand_bbox(size, lam):  # size : [Batch_size, Channel, Width, Height]
+    W = size[2]
+    H = size[3]
+    cut_rat = np.sqrt(1. - lam)  # 패치 크기 비율
+    # cut_w = np.int(W * cut_rat)
+    cut_h = np.int(H * cut_rat)
+
+    # 패치의 중앙 좌표 값 cx, cy
+    # cx = np.random.randint(W)
+    cy = np.random.randint(H)
+
+    # 패치 모서리 좌표 값
+    bbx1 = 0
+    bby1 = np.clip(cy - cut_h // 2, 0, H)
+    bbx2 = W
+    bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+    return bbx1, bby1, bbx2, bby2
+
+def cutmix(batch, alpha, vertical, vertical_half):
+    data, targets = batch
+
+    if vertical_half:
+        lam = 0.5
+    else:
+        lam = np.random.beta(alpha, alpha)
+
+    if not vertical:
+        indices = torch.randperm(data.size(0))
+        shuffled_data = data[indices]
+        shuffled_targets = targets[indices]
+
+        image_h, image_w = data.shape[2:]
+        cx = np.random.uniform(0, image_w)
+        cy = np.random.uniform(0, image_h)
+        w = image_w * np.sqrt(1 - lam)
+        h = image_h * np.sqrt(1 - lam)
+        x0 = int(np.round(max(cx - w / 2, 0)))
+        x1 = int(np.round(min(cx + w / 2, image_w)))
+        y0 = int(np.round(max(cy - h / 2, 0)))
+        y1 = int(np.round(min(cy + h / 2, image_h)))
+
+        data[:, :, y0:y1, x0:x1] = shuffled_data[:, :, y0:y1, x0:x1]
+        targets = (targets, shuffled_targets, lam)
+
+        return data, targets
+    else:
+        rand_index = torch.randperm(data.size(0))
+        target_a = targets  # 원본 이미지 label
+        target_b = targets[rand_index]  # 패치 이미지 label
+        bbx1, bby1, bbx2, bby2 = rand_bbox(data.size(), lam)
+        data[:, :, bbx1:bbx2, bby1:bby2] = data[rand_index, :, bbx1:bbx2, bby1:bby2]
+        lam = 1 - ((bbx2 - bbx1) * (bby2 - bby1) / (data.size()[-1] * data.size()[-2]))
+        targets = (target_a, target_b, lam)
+        return data, targets
 
 
 def seed_everything(seed):
@@ -108,14 +153,12 @@ def increment_path(path, exist_ok=False):
 
 
 def train(data_dir, model_dir, args):
-    wandb.init(project="hyunjin", entity="shine_light", config = {
-        "learning_rate": 0.001,
-        "epochs": 30,
-        "batch_size": 64  
-    })
-    save_dir = increment_path(args.name)
-    wandb.run.name = save_dir
-    wandb.run.save()
+    # wandb.init(project="hyunjin", entity="shine_light", config = {
+    #     "learning_rate": 0.001,
+    #     "epochs": 30,
+    #     "batch_size": 64  
+    # })
+    # wandb.run.name = 'Vgg19-ExponentialLR-1'
     # # wandb.watch(model) 옵션
     seed_everything(args.seed)
 
@@ -147,14 +190,14 @@ def train(data_dir, model_dir, args):
         std=dataset.std,
     )
     dataset.set_transform(transform)
+
     # -- data_loader
     train_set, val_set = dataset.split_dataset()
 
     train_loader = DataLoader(
         train_set,
         batch_size=args.batch_size,
-        # num_workers=multiprocessing.cpu_count() // 2,
-        num_workers=1,
+        num_workers=multiprocessing.cpu_count() // 2,
         shuffle=True,
         pin_memory=use_cuda,
         drop_last=True,
@@ -183,7 +226,7 @@ def train(data_dir, model_dir, args):
         weight_decay=5e-4,
     )
     # StepLR
-    # scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.5)
+    scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.5)
 
     # CyclicLR
     # scheduler = CyclicLR(optimizer, base_lr=0.001, max_lr=0.1, step_size_up=4, step_size_down=6, mode='triangular',cycle_momentum=False)
@@ -193,13 +236,13 @@ def train(data_dir, model_dir, args):
     # scheduler = ExponentialLR(optimizer, gamma=0.95)
 
     # CosineAnnealingWarmRestarts
-    # scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=1, eta_min=0, last_epoch=-1)
+    # scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=150, T_mult=1, eta_min=0, last_epoch=-1)
 
     # Custom CosineAnnealingWarmRestarts
-    # scheduler = CosineAnnealingWarmUpRestarts(optimizer, T_0=10, T_mult=1, eta_max=0.0005,  T_up=3, gamma=0.5)
+    # scheduler = CosineAnnealingWarmUpRestarts(optimizer, T_0=20, T_mult=1, eta_max=0.005,  T_up=6, gamma=0.5)
 
     # CosineAnnealingLR
-    scheduler = CosineAnnealingLR(optimizer, eta_min=0.001,T_max=3)
+    # scheduler = CosineAnnealingLR(optimizer, eta_min=0.001,T_max=3)
 
     # plateau
     # scheduler = ReduceLROnPlateau(optimizer,'min')
@@ -213,51 +256,56 @@ def train(data_dir, model_dir, args):
     best_val_acc = 0
     best_val_f1 = 0
     best_val_loss = np.inf
-    # early stopping
-    patience = 4
-    counter = 0
-    example_imgaes = []
-    grad_cams = []
-
     for epoch in tqdm(range(args.epochs)):
         # train loop
         model.train()
         loss_value = 0
         matches = 0
         f1=0
+        target_list = [[],[]]
+        pred_target_list = []
         for idx, train_batch in enumerate(train_loader):
-            inputs, labels = train_batch
+            train_batch = cutmix(train_batch, 1, vertical=False, vertical_half = np.False_)
+
+            inputs, labels_cut = train_batch
             inputs = inputs.to(device)
-            labels = labels.to(device)
-            # # custom augmentation
-            # if args.augmentation == "CustomAugmentation":
-            #     aug_inputs = []
-            #     for input, label in zip(inputs, labels):
-            #         if label not in [0, 1, 3, 4]:
-            #             aug_input = augmentation(input)
-            #             aug_inputs.append(aug_input)
-            #         else:
-            #             aug_inputs.append(input)
-            #     inputs = torch.stack(aug_inputs)
+            labels,target,lam = (labels_cut[0].to(device), labels_cut[1].to(device), labels_cut[2])
+            target_list[0] += labels.tolist()
+            target_list[1] += target.tolist()
+
             optimizer.zero_grad()
 
             outs = model(inputs)
-            preds = torch.argmax(outs, dim=-1)
-            
-            loss = criterion(outs, labels)
-
+            # preds = torch.argmax(outs, dim=-1)     
+            preds = torch.softmax(outs, dim=-1)    
+            loss_func =  CrossEntropyLoss(reduction='mean')
+            loss = lam * loss_func(preds, labels) + (1 - lam) * loss_func(preds, target)
             loss.backward()
             # gradient clipping
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=5)
-            optimizer.step()
 
+            optimizer.step()
+            # cutmix acc
+            arg_preds = torch.argmax(preds, dim=1)
+            correct1 = (arg_preds == labels).sum().item()
+            correct2 = (arg_preds == target).sum().item()
+            accuracy = (lam * correct1 + (1 - lam) * correct2)
+            pred_target_list += arg_preds.tolist()
+            # cutmix acc end
             loss_value += loss.item()
-            matches += (preds == labels).sum().item()  
+            matches += accuracy
+
             # add f1
-            pred_f1 = torch.clone(preds).detach().cpu().numpy()
+            # print('preds',preds)
+            # print('arg_preds',arg_preds)
+            pred_f1 = torch.clone(arg_preds).detach().cpu().numpy()
             label_f1 = torch.clone(labels).detach().cpu().numpy()
             # print(f1_score(label_f1, pred_f1, average="macro"))
-            f1 += f1_score(label_f1, pred_f1, average="macro")
+
+            # f1 += f1_score(label_f1, pred_f1, average="macro")
+            # print('pred_f1',pred_f1, 'label_f1',label_f1)
+            f1 += f1_score(target_list[0] , pred_target_list, average="macro") * lam + f1_score(target_list[1] , pred_target_list, average="macro") * (1 - lam)
+
             if (idx + 1) % args.log_interval == 0:
                 train_loss = loss_value / args.log_interval
                 train_acc = matches / args.batch_size / args.log_interval
@@ -282,7 +330,7 @@ def train(data_dir, model_dir, args):
                 matches = 0
                 f1 = 0
 
-        scheduler.step()
+        # scheduler.step()
 
         # val loop
         with torch.no_grad():
@@ -291,41 +339,8 @@ def train(data_dir, model_dir, args):
             val_loss_items = []
             val_acc_items = []
             val_f1_items = []
-            # randint -> random image show in wandb
-            i = random.randint(0, len(inputs) - 1)
             # wandb
-            example_imgaes.append(
-                wandb.Image(
-                    inputs[i],
-                    caption="Pred: {} Truth: {}".format(preds[i].item(), labels[i]),
-                )
-            )
-            
-            # grad-cam
-            # def gradcam(inputs, i, model):
-            #     rgb_img = np.transpose(
-            #         np.float32(np.clip(inputs[i].cpu().numpy(), 0, 1)), (1, 2, 0)
-            #     )
-            #     target_layers = [model.module.target_layer()]
-            #     cam = GradCAM(
-            #         model=model, target_layers=target_layers, use_cuda=use_cuda
-            #     )
-            #     # print(torch.unsqueeze(inputs[i], 0))
-            #     grayscale_cam = cam(input_tensor=torch.unsqueeze(inputs[i], 0))
-            #     grayscale_cam = grayscale_cam[0, :]
-            #     visualization = show_cam_on_image(
-            #         rgb_img, grayscale_cam, use_rgb=True, colormap=cv2.COLORMAP_JET
-            #     )
-            #     return visualization
-
-            # visualization = gradcam(inputs, i, model)
-            # grad_cams.append(
-            #     wandb.Image(
-            #         visualization,
-            #         caption="Pred: {} Truth: {}".format(preds[i].item(), labels[i]),
-            #     )
-            # )
-
+            example_imgaes = []
             figure = None
             for val_batch in val_loader:
                 inputs, labels = val_batch
@@ -381,10 +396,7 @@ def train(data_dir, model_dir, args):
                 counter = 0
             else:
                 counter +=1
-            # early stopping
-            if counter > patience:
-                print("Early Stopping...")
-                break
+
             torch.save(model.module.state_dict(), f"{save_dir}/last.pth")
             print("-" * 100)
             print(
@@ -396,11 +408,7 @@ def train(data_dir, model_dir, args):
             logger.add_scalar("Val/f1", val_f1, epoch)
             logger.add_figure("results", figure, epoch)
             print()
-            wandb.log({"acc": val_acc, 
-            "loss" : val_loss, 
-            "f1-score" : val_f1,
-            # "Grad_CAM": grad_cams,
-            })
+            # wandb.log({"acc": val_acc, "loss" : val_loss, "f1-score" : val_f1})
             
 
 
@@ -486,7 +494,7 @@ if __name__ == "__main__":
         help="how many batches to wait before logging training status",
     )
     parser.add_argument(
-        "--name", default="exp", help="model save at {SM_MODEL_DIR}/{name}"
+        "--name", default=f"{model_name}_{batch_size}_", help="model save at {SM_MODEL_DIR}/{name}"
     )
 
     # Container environment
